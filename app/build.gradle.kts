fd8964a749b25399bc26a987c5cdae85ff3d6a2d
Oleksandr073 <oleksandr073403@gmail.com>
2025-08-05 10:28:47 +0300

    Update target SDK version from 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.oleksandr073.localnotes"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.oleksandr073.localnotes"
         minSdk = 34
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
